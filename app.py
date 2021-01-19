7893ff497352b1c282418a95b9f2eef330d1f074
spe301 <56374336+spe301@users.noreply.github.com>
2021-01-19 17:59:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-from tensorflow.keras import preprocessing, models
+from keras import preprocessing, models
 
 def ModelReadyString(text_str, pad):
     '''converts an individual unit of text into tokenized sequences'''
