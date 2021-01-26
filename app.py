e11dd8970fff546e67ca32df4d8aff45e6d23488
Spencer Holley <jeppyspence@gmail.com>
2021-01-26 14:48:13 -0600

    fixed indentation

--- app.py
+++ app.py
@@ -36,15 +36,15 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-	user_input = request.form['Text']
-	if len(user_input) < 56:
-		try:
-			text = GetText(user_input) 
-			tokenized = ModelReadyString(text, 1720)
-		except:
-			return "User input must have at least 56 characters if it's not a url"
-	else:
-    	tokenized = ModelReadyString(user_input, 1720)
+    user_input = request.form['Text']
+    if len(user_input) < 56:
+        try:
+            text = GetText(user_input) 
+            tokenized = ModelReadyString(text, 1720)
+        except:
+            return "User input must have at least 56 characters if it's not a url"
+    else:
+        tokenized = ModelReadyString(user_input, 1720)
     prediction = model.predict(tokenized)
     output = round(prediction[0][1]*100, 2)
     return render_template('index.html', prediction_text='We predict a {}% probability that an AI wrote this.'.format(output))
